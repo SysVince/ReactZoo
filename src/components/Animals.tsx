1a93d0abde5044e65b45e1cf77518450769024e3
sys.vince <sys.vince@gmail.com>
2022-05-12 19:57:01 +0200

    Animals.tsx hade gammal kod :/

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,127 +1,72 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { IAnimal } from "../models/IAnimal"
-import { IExtendedAnimal } from "../models/IExtendedAnimal";
+import { IAnimal } from "../models/IAnimal";
+import { StyledButton } from "./StyledButton";
 
 const StyledDivContainer = styled.div`
-
-justify-content: center;
-width: 35rem;
-text-align: center;
-margin: 10px;
-img {
- max-width: 100%;
- max-height: 100%;
-}
+  justify-content: center;
+  width: 35rem;
+  text-align: center;
+  margin: 10px;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
 `;
 
-// Flytta denna save function till Animal.tsx
-export function saveAnimalsList(animals:IAnimal[]){
-
-    animals.forEach( (animal) => {
-    animal.lastFed = animal.lastFed.slice(0, 19);
-    });
-        localStorage.setItem("animals", JSON.stringify(animals));
-}
-
 export const Animals = () => {
-    const [animals, setAnimals] = useState<IAnimal[]>([])
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
 
+  useEffect(() => {
+    if (animals.length !== 0) return;
 
-    useEffect( () => {
-        if (animals.length !==0) return;
+    axios
+      .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
+      .then((response) => {
+        setAnimals(response.data);
+      });
+  });
 
-        axios.get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
-        .then( (response) => {
-            setAnimals(response.data);
-           
-        });
-
-    });
-
-    useEffect(() => {
-        if (localStorage.getItem("animals") === null && animals.length !== 0) {
-          animals.forEach((animal) => {
-            animal.lastFed = animal.lastFed.slice(0, 19);
-          });
-          localStorage.setItem("animals", JSON.stringify(animals));
-        }
-      }, [animals]);
- 
-  
-
-
-    function checkAnimalsToFeed(){
-        const animalsFromLS = JSON.parse(localStorage.getItem("animals") || "[]");
-        let animalFeedingNeeded = "";
-        animalsFromLS.forEach( (animal:IExtendedAnimal) => {
-            if(Date.parse(Date()) - Date.parse(animal.lastFed) > 15000){
-                animalFeedingNeeded += animal.name + ", "
-                
-            }
-        });
-        alert("Dessa djur behöver matas: " + animalFeedingNeeded);
-
+  useEffect(() => {
+    if (localStorage.getItem("animals") === null && animals.length !== 0) {
+      animals.forEach((animal) => {
+        animal.lastFed = animal.lastFed.slice(0, 19);
+      });
+      localStorage.setItem("animals", JSON.stringify(animals));
     }
+  }, [animals]);
 
-
-
-        
-
-
-//let dateNowsss = new Date((new Date().toISOString()).slice(0,19));
-//   console.log("added 2h",(Date.parse(dateNow)+7200000));
-//    console.log("dateNow ",dateNow);
-//   new Date().getSeconds()
-//  console.log("test: ", new Date((new Date().toISOString())).toISOString());
-// console.log(new Date(new Date().toString().split("GMT")[0]+" UTC").toISOString());
-// let dateNow = (new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString()).slice(0,19)
-//   console.log(dateNow);
-
- 
-//  alert("Testing 1 time");
-
-// saveAnimalsLS([...animals]);
-
-    let animalsHtml = animals.map( (animal) => {
-
-        // Date.parse(Date()) - Date.parse(animal.lastFed)
-
-        return (
-            <StyledDivContainer key={animal.id}>
-                <Link to={"/Animal/" + animal.id}>
-                <h3>{animal.name}</h3>
-                <p>{animal.shortDescription}</p>
-                <img src={animal.imageUrl} alt={animal.name + " Image"}/>
-                </Link>
-
-            </StyledDivContainer>);
+  function checkAnimalsToFeed() {
+    const animalsFromLS = JSON.parse(localStorage.getItem("animals") || "[]");
+    let animalFeedingNeeded = "";
+    animalsFromLS.forEach((animal: IAnimal) => {
+      if (Date.parse(Date()) - Date.parse(animal.lastFed) > 4000 * 60 * 60) {
+        animalFeedingNeeded += animal.name + ", ";
+      }
     });
+    alert("Dessa djur behöver matas: " + animalFeedingNeeded);
+  }
 
-    // console.log(Date());
-    
-    // return (<>
-    // {animals.map( (animal) => {
+  let animalsHtml = animals.map((animal) => {
+    return (
+      <StyledDivContainer key={animal.id}>
+        <Link to={"/Animal/" + animal.id}>
+          <h3>{animal.name}</h3>
+          <p>{animal.shortDescription}</p>
+          <img src={animal.imageUrl} alt={animal.name + " Image"} />
+        </Link>
+      </StyledDivContainer>
+    );
+  });
 
-    //     return (
-    //         <StyledDivContainer key={animal.id}>
-    //             <Link to={"/Animal/" + animal.id}>
-    //             <h3>{animal.name}</h3>
-    //             <p>{animal.shortDescription}</p>
-    //             <img src={animal.imageUrl} alt={animal.name}/>
-                
-    //             </Link>
-    //             <button type="button" onClick={() => {saveAnimalsList(animals)}}>SAVE ALL ANIMALS TEST</button>
-    //         </StyledDivContainer>);
-    // })}
-
-    //     </>);
-
-return (<>
-<div><button type="button" onClick={checkAnimalsToFeed}>Kolla vilka djur som behöver matas</button></div>
-{animalsHtml}
-</>);
-
-}
+  return (
+    <>
+      <div>
+          <StyledButton color="teal" onClick={checkAnimalsToFeed}>Kolla vilka djur som behöver matas</StyledButton>
+      </div>
+      {animalsHtml}
+    </>
+  );
+};
